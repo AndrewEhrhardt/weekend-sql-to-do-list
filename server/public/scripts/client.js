47a5e3a48ab00cd05ffa7aab79222a639032ad15
AndrewEhrhardt <andyehrhardt@gmail.com>
2021-09-10 18:21:01 -0500

    completed button is now a toggle input that can be unchecked

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -9,7 +9,7 @@ function readyNow(){
 
 function clickListener(){
     $('#add-task').on('click', postTask)
-    $('#task-list').on('click', '.mark-completed-button', markCompleted)
+    $('#task-list').on('click', '.completed-toggle', toggleCompleted)
     $('#task-list').on('click', '.delete-button', deleteTask)
 }
 
@@ -50,11 +50,9 @@ function appendTasksToDom(response){
         $('#task-list').append(`
         <tr>
         <td class="${(task.completed ? "completed": "")}">${task.task}</td>
-        <td>${task.completed ? "":
-            `<button data-id="${task.id}" class="mark-completed-button">
-                 Completed
-            </button>` 
-        }
+        <td> 
+            <input type="checkbox" data-id="${task.id}" class="completed-toggle" ${(task.completed ? "checked": "")}>
+        </td>
         <td>
             <button data-id="${task.id}" class="delete-button">
             Delete
@@ -64,11 +62,19 @@ function appendTasksToDom(response){
     `)
 }
 
-function markCompleted(){
+function toggleCompleted(){
     const taskId = $(this).data('id');
+    const toggleState = {};
+    if ($(this).attr('checked') == "checked"){ //checks if the box is checked 
+        toggleState.toggle = false; //since the checkbox is being unchecked, the task completed status will now be false 
+    } else {
+        toggleState.toggle = true; //since box is being checked, task completed will now be set to true
+    }
+    console.log(toggleState.toggle);
     $.ajax({
         method: 'PUT',
         url: `/tasks/${taskId}`,
+        data: toggleState,
     }).then( function(response){
         console.log('Task Completed!')
         getTasks();
--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -31,9 +31,10 @@ router.get('/', (req, res) => {
 
 router.put('/:id', (req,res) => {
   console.log(req.params);
+  let toggleState = req.body;
   const taskId = req.params.id;
-  const queryText = 'UPDATE "tasks" SET "completed" = True WHERE "id" = $1;';
-  pool.query(queryText, [taskId]).then((result) => {
+  const queryText = 'UPDATE "tasks" SET "completed" = $2 WHERE "id" = $1;';
+  pool.query(queryText, [taskId, toggleState.toggle]).then((result) => {
       res.sendStatus(200);
   }).catch((error) => {
       console.log('Error in /tasks PUT', error);
